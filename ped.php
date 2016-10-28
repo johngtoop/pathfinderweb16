dfcb3f0ca53307019800c4f36077246fad8342ce
John Toop <johngtoop@live.ca>
2016-10-27 22:57:05 -0400

    Add files via upload

--- ped.php
+++ ped.php
@@ -5,33 +5,110 @@
 	$club = $_SESSION['pmClub'];
 	$name = "";
 	$email = "";
-	$ssql = ".";
+	$ssql = "+";
 	$active = "";
 	$trole = "0";
-	$tfamily = 99;
+	$tpm = "0";              // temp member
+	$tfamily = 99;          // temp family
 	echo "<br>role = " . $role;
 	echo "<br>Club = " . $club;
 
 	if (($role == 6) || ($role == 2) || ($role == 3)) {
 		echo tophtml("Edit Members");
-		echo "<br /><a href='apath.php'>Pathfinder Home</a>";
+		if (!$_POST) {
+			echo selectclub($club) . " " . selectpm($tpm) . " <input type='submit' value='refresh' src='refresh.gif' name='refresh' />\n";
+			echo "<input type='submit' />";
+		} else {
+			if ($_POST['refresh']) {
+				$ssql = "refresh pressed.";
+			} else {
+				$ssql = "refresh not pressed";
+			}
+			echo selectclub($club) . " " . selectpm($tpm) . " <input type='submit' value='refresh' src='refresh.gif' name='refresh' />\n";
+			echo "<input type='submit' />";
+		}
+		echo "<br /><a href='apath.php'>Pathfinder Home</a>\n";
+		echo "<br />ssql=" . $ssql;
 		echo "</form></body></html>";
 	} else {
 		echo "Sorry .. not enough permissions.<br />";
 		echo "<a href='../pf.php'>Home</a>";
 	}
 
+	// html for top of page
 	function tophtml($titl) {
-		$t = "<!DOCTYPE html>";
-		$t = $t . "<html lang='en'>";
-		$t = $t . "<head>";
-		$t = $t . "<meta charset='UTF-8'>";
-		$t = $t . "<title>" . $titl . "</title>";
-		$t = $t . "<link rel='stylesheet' href='zpa.css'>";
-		$t = $t . "</head>";
-		$t = $t . "<body>";
-		$t = $t . "<form action='' method='post'>";
-		$t = $t . "<h2>" . $titl . "</h2>";
+		$t = "<!DOCTYPE html>\n";
+		$t .= "<html lang='en'>\n";
+		$t .= "<head>\n";
+		$t .= "<meta charset='UTF-8'>\n";
+		$t .= "<title>" . $titl . "</title>\n";
+		$t .= "<link rel='stylesheet' href='zpa.css'>\n";
+		$t .= "</head>\n";
+		$t .= "<body>\n";
+		$t .= "<form action='' method='post'>\n";
+		$t .= "<h2>" . $titl . "</h2>\n";
 		return $t;
+	}
+	
+	// Select (dropdown to all clubs)
+	function selectclub($dftclub) {
+		//SELECT pcId, pcName FROM pfClubs ORDER BY pcName
+		require 'dbinfo.php';
+		$linkc = mysqli_connect($dbserver, $dbuser, $dbpass, $database);
+		
+		// Check connection
+		if($linkc === false){
+			die("ERROR: Could not connect. " . mysqli_connect_error());
+		}		 
+		// Attempt select query execution
+		$sql = "SELECT pcId, pcName FROM paClubs ORDER BY pcName";
+		if($resultc = mysqli_query($linkc, $sql)){
+			$ss = "";
+			if(mysqli_num_rows($resultc) > 0){
+				$ss = "<select name='pmClub'>\n";
+				while($rowc = mysqli_fetch_array($resultc)){
+					if ($rowc['pcId']===$dftclub) {
+						$ss .= "<option selected value='" . $rowc['pcId'] . "'>" . $rowc['pcName'] . "</option>\n";
+					} else {
+						$ss .= "<option value='" . $rowc['pcId'] . "'>" . $rowc['pcName'] . "</option>\n";
+					}
+				}
+				$ss .= "</select>\n";
+				// Close result set
+				mysqli_free_result($resultc);
+			} 
+		}
+		mysqli_close($linkc);
+		return $ss;
+	}
+	
+	// Select pathfinder member
+	function selectpm($pm) {
+		
+		require 'dbinfo.php';
+		$linkm = mysqli_connect($dbserver, $dbuser, $dbpass, $database);
+		
+		// Check connection
+		if($linkm === false){
+			die("ERROR: Could not connect. " . mysqli_connect_error());
+		}		 
+		// Attempt select query execution
+		$sql = "SELECT pmId, pmName, pmMail FROM paMembrs ORDER BY UPPER(pmName)";
+		if($resultm = mysqli_query($linkm, $sql)){
+			$ss = "";
+			if(mysqli_num_rows($resultm) > 0){
+				$ss .= "<select name='members'>\n";
+				while($rowm = mysqli_fetch_array($resultm)){
+					if ($rowm['pmId']==$pm)
+						$ss .= "<option value='" . $rowm['pmId'] . "' selected>" . $rowm['pmName'] . " " . $rowm['pmMail'] . "</option>\n";
+					else
+						$ss .= "<option value='" . $rowm['pmId'] . "'>" . $rowm['pmName'] . " " . $rowm['pmMail'] . "</option>\n";
+				}
+				$ss .= "</select>\n";
+				mysqli_free_result($resultm); 		// Close result set
+			} 
+		}		
+		mysqli_close($linkm); // Close connection
+		return $ss;
 	}
 ?>
